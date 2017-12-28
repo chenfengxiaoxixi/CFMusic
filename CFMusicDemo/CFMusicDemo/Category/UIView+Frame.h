eaf237905141a401c1a2a9f0ca1f0c5988c246b5
cfxxx <179623459@qq.com>
2017-12-28 23:17:30 +0800

    优化

---
+++ CFMusicDemo/CFMusicDemo/Category/UIView+Frame.h
@@ -0,0 +1,20 @@
+//
+//  UIView+Frame.h
+//  CFBarrage
+//
+//  Created by cf on 2017/12/16.
+//  Copyright © 2017年 chenfeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (Frame)
+
+@property (nonatomic, assign) CGFloat x;
+@property (nonatomic, assign) CGFloat y;
+ @property (nonatomic, assign) CGFloat width;
+ @property (nonatomic, assign) CGFloat height;
+ @property (nonatomic, assign) CGPoint origin;
+ @property (nonatomic, assign) CGSize size;
+
+@end
---
+++ CFMusicDemo/CFMusicDemo/Category/UIView+Frame.m
@@ -0,0 +1,84 @@
+//
+//  UIView+Frame.m
+//  CFBarrage
+//
+//  Created by cf on 2017/12/16.
+//  Copyright © 2017年 chenfeng. All rights reserved.
+//
+
+#import "UIView+Frame.h"
+
+@implementation UIView (Frame)
+
+- (void)setX:(CGFloat)x
+ {
+    CGRect frame = self.frame;
+         frame.origin.x = x;
+         self.frame = frame;
+}
+
+- (CGFloat)x {
+    return self.frame.origin.x;
+}
+
+- (void)setY:(CGFloat)y
+{
+         CGRect frame = self.frame;
+         frame.origin.y = y;
+         self.frame = frame;
+}
+
+- (CGFloat)y
+{
+         return self.frame.origin.y;
+}
+
+ - (void)setOrigin:(CGPoint)origin
+ {
+         CGRect frame = self.frame;
+         frame.origin = origin;
+         self.frame = frame;
+}
+
+ - (CGPoint)origin
+ {
+         return self.frame.origin;
+}
+
+ - (void)setWidth:(CGFloat)width
+ {
+         CGRect frame = self.frame;
+         frame.size.width = width;
+         self.frame = frame;
+}
+
+ - (CGFloat)width
+ {
+         return self.frame.size.width;
+}
+
+ - (void)setHeight:(CGFloat)height
+ {
+         CGRect frame = self.frame;
+         frame.size.height = height;
+         self.frame = frame;
+}
+
+ - (CGFloat)height
+ {
+         return self.frame.size.height;
+}
+
+ - (void)setSize:(CGSize)size
+ {
+         CGRect frame = self.frame;
+         frame.size = size;
+         self.frame = frame;
+}
+
+- (CGSize)size
+{
+         return self.frame.size;
+}
+
+@end
